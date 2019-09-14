27cf17239da11c5cf0a3b469f3f26e943329bc8d
Dustin_chavez <dustin_chavez@cs104u-5f.acad.fullerton.edu>
2019-09-14 14:07:45 -0700

    Predicted next year sales

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -1,1 +1,19 @@
 // Sales prediction
+#include <iostream>
+
+int main()
+{
+  long int my_sales2;
+  const double PERCENTAGE = 1.20;
+  const long int LASTYEAR = 2103419277;
+  //find sales
+  
+
+
+  my_sales2 = LASTYEAR * PERCENTAGE;
+  std::cout << "Last years sales were $" << LASTYEAR << std::endl;
+  std::cout << "This years sales prediction is $" << my_sales2 << std::endl;
+
+  return 0;
+
+}
